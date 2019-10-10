c4e2604247c12ec1404af63918be229fac131d1d
antoniomesquita09 <antoniomesquita09@gmail.com>
2019-10-10 11:39:00 -0300

    Wip: read file

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -2,9 +2,105 @@ const path = require("path");
 const fs = require("fs");
 
 class FileController {
-  show(req, res) {
-    const text = fs.readFileSync(path.resolve(__dirname, "start.txt"), "utf8");
-    return res.json({ data: text });
+  index(req, res) {
+    const { filename: file } = req.file;
+
+    const filePath = path.resolve(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "tmp",
+      "uploads",
+      file
+    );
+    const fileText = fs.readFileSync(filePath, "utf8");
+
+    const phraseArray = fileText.split("\n");
+
+    // console.log(phraseArray);
+
+    const teams = [
+      "flamengo",
+      "santos",
+      "palmeiras",
+      "corinthians",
+      "sao paulo",
+      "internacional",
+      "gremio",
+      "bahia",
+      "athletico-pr",
+      "goias",
+      "atletico-mg",
+      "botafogo",
+      "fortaleza",
+      "vasco",
+      "fluminense",
+      "csa",
+      "ceara",
+      "cruzeiro",
+      "avai",
+      "chapecoense"
+    ];
+
+    /* var i;
+    for (i = 0; i < phraseArray.length; i++) {
+      // text += cars[i] + "<br>";
+      console.log(phraseArray[i]);
+      function checkTeams(phrase) {
+        return phrase == "1111";
+      }
+      if (phraseArray.find(checkTeams)) console.log("tem 1");
+    } */
+
+    phraseArray.forEach(filter);
+
+    function filter(item, index) {
+      let phrase = item
+        .toLowerCase()
+        .replace(/á|à|ã|â/g, "a")
+        .replace(/é|è|ê/g, "e")
+        .replace(/í|ì/g, "i")
+        .replace(/ó|ò|õ/g, "o");
+
+      // console.log(phrase);
+      let isTeam = false;
+      isTeam = teams.find(element => element === phrase);
+      // console.log(`Nome de time: ${phrase}\n`);
+      //return false
+
+      const invalidPhrasesPath = path.resolve(
+        __dirname,
+        "..",
+        "..",
+        "..",
+        "tmp",
+        "uploads",
+        "invalidPhrases"
+      );
+
+      if (isTeam) {
+        fs.writeFile(invalidPhrasesPath, `${isTeam}\n`, function(err) {
+          if (err) {
+            return console.log(err);
+          }
+
+          console.log("The file was saved!");
+        });
+      }
+    }
+
+    // const filePath = path.resolve(__dirname, file);
+
+    // console.log(filePath);
+
+    // const text = fs.readFileSync(filePath, "utf8");
+
+    // console.log(text);
+
+    fs.unlinkSync(filePath);
+
+    return res.json({ data: "good" });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
+const multerConfig = require("./config/multer");
+const upload = require("multer")(multerConfig);
 
 const routes = new Router();
 
 const FileController = require("./app/controllers/FileController");
 
-routes.get("/test", FileController.show);
+routes.post("/test", upload.single("file"), FileController.index);
+
 routes.get("/", (req, res) => {
   return res.json({ test: "uau" });
 });
